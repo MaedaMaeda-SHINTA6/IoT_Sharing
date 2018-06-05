25e1d06fb80e419bc8ebefd04c2fac9f91cf5804
Maeda <maeda.shintaro@jp.fujitsu.com>
2018-06-05 11:33:47 +0900

    node.jsを追加しました

--- html/server.js
+++ html/server.js
@@ -1,8 +1,33 @@
-var express = require('express');
-var app = express();
- 
-app.get('/', function (req, res) {
-  res.send('Hello Express!');
-});
- 
-app.listen(3000);
+var http = require('http');
+var fs   = require('fs');
+var path = require('path');
+var mime = {
+  ".html": "text/html",
+  ".css":  "text/css"
+  // 読み取りたいMIMEタイプはここに追記
+};
+
+var http_server = new http.createServer(function(req, res) {
+
+  if (req.url == '/') {
+    filePath = '/index.html';
+  } 
+  else if(req.url=='/login')
+  {
+    filePath='/sign_in.html';
+  }
+  else {
+    filePath = req.url;
+  }
+  var fullPath = __dirname + filePath;
+
+  res.writeHead(200, {"Content-Type": mime[path.extname(fullPath)] || "text/plain"});
+  fs.readFile(fullPath, function(err, data) {
+    if (err) {
+      // エラー時の応答
+    } else {
+      res.end(data, 'UTF-8');
+    }
+  });
+}).listen(3000);
+console.log('Server running at http://localhost:3000/');
