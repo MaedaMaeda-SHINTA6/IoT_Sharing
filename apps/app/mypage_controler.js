94dab727ea8436e4a7df90e57afb5b589f720abd
desucru <desucru@gmail.com>
2018-07-19 20:10:06 +0900

    MyPage更新処理実装

--- apps/app/mypage_controler.js
+++ apps/app/mypage_controler.js
@@ -27,6 +27,7 @@ exports.get = function (req, res) {
             if (response.body) {
               categories = response.body;
               console.log(categories);
+              console.log(categories['Accounts'][0]['AccountExtensions']);
               var params = {
                 "MemberId": categories['Accounts'][0]['id'],
                 "Account": categories['Accounts'][0]['userId'],
@@ -34,6 +35,7 @@ exports.get = function (req, res) {
                 "LastLoginTime": categories['Accounts'][0]['lastLoginDatetime'],
                 "role": categories['Accounts'][0]['role'],
                 "displayName": categories['Accounts'][0]['AccountExtensions'][0]['value'],
+                "extensions_id": categories['Accounts'][0]['AccountExtensions'][0]['extensionCategoryId'],
                 "param": "1"
               };
               callback(null);
@@ -51,7 +53,73 @@ exports.get = function (req, res) {
 };
 
 exports.update = function (req, res) {
-  console.log(req.body);
-  res.redirect('/mypage');
+  var error = [];
+  async.waterfall([
+    function (callback) {
+      var requestBody = {
+        'extensions_id': req.body.extensions_id,
+        'MemberId': req.body.MemberId,
+        'Account': req.body.Account,
+        'MailAddress': req.body.MailAddress,
+        'role': req.body.role,
+        'displayName': req.body.displayName,
+        'old_password': req.body.old_password,
+        'new_password1': req.body.new_password1,
+        'new_password2': req.body.new_password2
+      }
+
+      callback(null, requestBody);
+
+    },
+    function (requestBody, callback) {
+      if (requestBody.new_password1 != requestBody.new_password2) {
+        res.render('error', { 'message': '新しいパスワードが一致しません。' });
+      } else {
+        var putBodyData = {
+          "userId": requestBody.Account,
+          "newPassword": requestBody.new_password1,
+          "oldPassword": requestBody.old_password,
+          "mailAddress": requestBody.MailAddress,
+          "status": 0,
+          "AccountExtensions": [
+            {
+              "extensionCategoryId": requestBody.extensions_id,
+              "dataType": 20,
+              "value": requestBody.displayName,
+              "publicLevel": 0
+            }
+          ]
+        };
+        var id = requestBody.MemberId;
+        callback(null, putBodyData,id);
+      }
+    },
+    function (putBodyData, id,callback) {
+
+      console.log(id);
+
+      var requestData = common.createPutRequest("accounts",id,putBodyData);
+      console.log(requestData);
+      var putdatas;
+      request.put(requestData,
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            if (response.body) {
+              putdatas = response.body;
+              callback(null, putdatas);
+            }
+          } else {
+            common.outputError(error, response);
+          }
+        }
+      );
+      callback(error, putdatas);
+    }
+  ],
+    function (err, putdatas) {
+      console.log("async end");
+      res.redirect('/mypage');
+    }
+  );
 };
 
